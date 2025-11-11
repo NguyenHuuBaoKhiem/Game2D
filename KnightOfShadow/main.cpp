34aef5bc0c33b2c04a0be077148dd90634b9b9b5
NguyenHuuBaoKhiem <ajk50250@gmail.com>
2025-11-11 23:12:12 +0700

    update main

--- KnightOfShadow/main.cpp
+++ KnightOfShadow/main.cpp
@@ -2,6 +2,7 @@
 #include "Player.h"
 #include "Enemy1.h"
 #include "Enemy2.h"
+#include "Enemy3.h"
 #include "EnemyManager.h"
 #include "MenuScreen.h"
 
@@ -13,53 +14,67 @@ int main() {
     MenuScreen menu(window);
 
     // --- MAP ---
-    Map map;
-    map.Load("Assets/Images/Map/Map.ldtk", "Level_0");
+    //Map map;
+    //map.Load("Assets/Images/Map/Map.ldtk", "Level_0");
 
     // --- PLAYER ---
-    sf::Texture texIdle, texWalk, texAttack1, texAttack2, texAttack3;
+    sf::Texture texIdle, texWalk, texAttack1, texAttack2, texAttack3, texSkill1;
     texWalk.loadFromFile("Assets/Images/Player/run.png");
     texIdle.loadFromFile("Assets/Images/Player/idle.png");
     texAttack1.loadFromFile("Assets/Images/Player/at1.png");
     texAttack2.loadFromFile("Assets/Images/Player/at2.png");
     texAttack3.loadFromFile("Assets/Images/Player/at3.png");
+    texSkill1.loadFromFile("Assets/Images/Player/skill1.png");
 
     texIdle.setSmooth(true);
     texWalk.setSmooth(true);
     texAttack1.setSmooth(true);
     texAttack2.setSmooth(true);
     texAttack3.setSmooth(true);
+    texSkill1.setSmooth(true);
 
-    Player player(texIdle, texWalk, texAttack1, texAttack2, texAttack3);
+    Player player(texIdle, texWalk, texAttack1, texAttack2, texAttack3, texSkill1);
     player.SetPosition(sf::Vector2f(200.f, 500.f));
 
+    // --- ENEMY MANAGER ---
     EnemyManager enemyManager;
 
-    // --- BOSS ---
+    //// --- BOSS 1 ---
     sf::Texture tIdle, tWalk, tAttack, tDeath, tAttack1, tAttack2;
-    if (!tIdle.loadFromFile("Assets/Images/Enemy/idle.png") ||
-        !tWalk.loadFromFile("Assets/Images/Enemy/walk.png") ||
-        !tAttack.loadFromFile("Assets/Images/Enemy/attack.png") ||
-        !tAttack1.loadFromFile("Assets/Images/Enemy/at1.png") ||
-        !tAttack2.loadFromFile("Assets/Images/Enemy/at2.png") ||
-        !tDeath.loadFromFile("Assets/Images/Enemy/death.png")) {
-        return 1;
-    }
+    tIdle.loadFromFile("Assets/Images/Enemy/idle.png");
+    tWalk.loadFromFile("Assets/Images/Enemy/walk.png");
+    tAttack.loadFromFile("Assets/Images/Enemy/attack.png");
+    tAttack1.loadFromFile("Assets/Images/Enemy/at1.png");
+    tAttack2.loadFromFile("Assets/Images/Enemy/at2.png");
+    tDeath.loadFromFile("Assets/Images/Enemy/death.png");
 
+    auto boss1 = std::make_unique<Enemy1>(tIdle, tWalk, tAttack, tAttack1, tAttack2, tDeath);
+    boss1->SetPosition({ 700.f, 500.f });
+    enemyManager.AddEnemy(std::move(boss1));
+
+    //// --- BOSS 2 ---
     sf::Texture tIdlee, tWalkk, tAttackk, tDeathh;
-    if (!tIdlee.loadFromFile("Assets/Images/Enemy/idle_1.png") ||
-        !tWalkk.loadFromFile("Assets/Images/Enemy/walk_1.png") ||
-        !tAttackk.loadFromFile("Assets/Images/Enemy/attack_1.png") ||
-        !tDeathh.loadFromFile("Assets/Images/Enemy/death_1.png")) {
-        return 1;
-    }
+    tIdlee.loadFromFile("Assets/Images/Enemy/idle_1.png");
+    tWalkk.loadFromFile("Assets/Images/Enemy/walk_1.png");
+    tAttackk.loadFromFile("Assets/Images/Enemy/attack_1.png");
+    tDeathh.loadFromFile("Assets/Images/Enemy/death_1.png");
 
-    auto boss = std::make_unique<Enemy1>(tIdle, tWalk, tAttack, tAttack1, tAttack2, tDeath);
-    boss->SetPosition({ 1000.f, 500.f });
-    enemyManager.AddEnemy(std::move(boss));
     auto boss2 = std::make_unique<Enemy2>(tIdlee, tWalkk, tAttackk, tDeathh);
     boss2->SetPosition({ 1000.f, 500.f });
     enemyManager.AddEnemy(std::move(boss2));
+
+    // --- BOSS 3 ---
+    sf::Texture tIdle3, tWalk3, tAttack3, tAttack13, tAttack23, tDeath3;
+    tIdle3.loadFromFile("Assets/Images/Enemy/idle_2.png");
+    tWalk3.loadFromFile("Assets/Images/Enemy/walk_2.png");
+    tAttack3.loadFromFile("Assets/Images/Enemy/attack_2.png");
+    tAttack13.loadFromFile("Assets/Images/Enemy/at1_2.png");
+    tAttack23.loadFromFile("Assets/Images/Enemy/at2_2.png");
+    tDeath3.loadFromFile("Assets/Images/Enemy/death_2.png");
+
+    auto boss3 = std::make_unique<Enemy3>(tIdle3, tWalk3, tAttack3, tAttack13, tAttack23, tDeath3);
+    boss3->SetPosition({ 1300.f, 500.f });
+    enemyManager.AddEnemy(std::move(boss3));
 
     sf::Clock clock;
 
@@ -106,16 +121,22 @@ int main() {
                 {
                     e->TakeDamage(20);
                 }
+                if (player.GetState() == PlayerState::Skill1 &&
+                    player.GetSkill1Hitbox().intersects(e->GetBodyHitbox()))
+                {
+                    e->TakeDamage(50);
+                }
             }
 
-            // --- Va chạm: Boss tấn công Player ---
-            // (Nếu bạn muốn boss gây damage)
-            // Giả sử Enemy1 có GetAttackBox() thì:
-            // if (boss.IsAttacking() && boss.GetAttackBox().intersects(player.GetBodyHitbox()))
-            //     player.TakeDamage(15);
+            for (const auto& e : enemyManager.GetEnemies())
+            {
+                // chỉ khi boss đang đánh và hitbox đang bật
+                if (e->IsAttacking() && e->GetAttackBox().intersects(player.GetBodyHitbox()))
+                {
+                    player.TakeDamage(15); // hoặc tuỳ từng skill
+                }
+            }
 
-            // --- Vẽ ---
-			map.Draw(window);
             player.Draw(window);
             enemyManager.DrawAll(window);
         }
